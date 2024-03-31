95eb7aa189817366a273b35a35d561812e028aad
myriam2601 <myriam.gazaoui@etu.hesge.ch>
2024-03-31 10:56:33 +0200

    Modification page d'accueil

--- routes/web.php
+++ routes/web.php
@@ -24,12 +24,7 @@ use Inertia\Inertia;
 
 // Route d'accueil
 Route::get('/', function () {
-    return Inertia::render('Welcome', [
-        'canLogin' => Route::has('login'),
-        'canRegister' => Route::has('register'),
-        'laravelVersion' => Application::VERSION,
-        'phpVersion' => PHP_VERSION,
-    ]);
+    return redirect()->route('login');
 });
 
 // Route du tableau de bord
