6bb3840b2f3b3973584c123f0eda3c13e00d7627
qadaidi <71253067+qadaidi@users.noreply.github.com>
2024-05-18 17:25:07 +0200

    Update DevisCard.jsx

--- resources/js/Components/DevisCard.jsx
+++ resources/js/Components/DevisCard.jsx
@@ -31,7 +31,7 @@ export default function DevisCards({ devis }) {
                 </h2>
                 <input
                     type="text"
-                    className="px-4 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 mb-4"
+                    className="px-4 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 mb-4 ml-2"
                     placeholder="Rechercher un devis..."
                     value={recherche}
                     onChange={(e) => setRecherche(e.target.value)}
